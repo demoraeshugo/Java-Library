fd6a064ed98c145293df2b41c0b40a9bd9baad3d
Hugo De Moraes <demoraeshugo@gmail.com>
2021-01-26 15:16:41 -0500

    Added project framework

---
+++ p1/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    private String number; //a 5-digit serial number unique to the book
+    private String name;
+    private boolean checkedOut;
+    private Date datePublished;
+    @Override
+    public boolean equals(Object obj){ }
+    @Override
+    public String toString() { }
+}
